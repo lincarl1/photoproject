143e39f73e20e328d434fcc6307edcc08d6bf73a
Kelsey T <kelsey.terrel@gmail.com>
2019-02-25 17:04:46 -0500

    Added test database URI

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   db: {
-    uri: '', //place the URI of your mongo database here.
+    uri: 'mongodb+srv://printsuser:uf2019swe@prints-cluster-cdipl.mongodb.net/test?retryWrites=true', //place the URI of your mongo database here.
   },
   port: process.env.PORT || 8080
 };
