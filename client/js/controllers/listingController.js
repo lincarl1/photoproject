cda39b1693fa10cadf9d9135737ad5fd09ab01ad
Carl Lin <lincarl@ufl.edu>
2019-03-22 14:57:44 -0400

    Added the PayPal Template, Basic Upload, and also Confirm Password

--- client/js/controllers/listingController.js
+++ client/js/controllers/listingController.js
@@ -12,6 +12,15 @@ angular.module('users').controller('UsersController', ['$scope', 'Users',
     // form to hold new user
     $scope.form = {};
 
+    function confirmPassword() {
+      var password1 = document.getElementById("psw").value;
+      var password2 = document.getElementById("pswconfirmation").value;
+      if (password1 != password2)
+      {
+        alert("Confirmation Password does not match Password!");
+      }
+    }
+
     $scope.addUser = function() {
       console.log("MADE IT HERE");
       //console.log($scope.newUser);
