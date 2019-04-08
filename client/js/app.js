a2e5e0bc5c9f96151257331640298c95f4f6ca1e
Kelsey T <kelsey.terrel@gmail.com>
2019-04-07 23:22:40 -0400

    Updated admin page tomatch website design

--- client/js/app.js
+++ client/js/app.js
@@ -7,41 +7,6 @@ angular.module('orders', []);
 var app = angular.module('printsApp', ['users', 'orders']);
 
 
-// allow ng-model to take in files
-/*
-//https://stackoverflow.com/questions/45548472/how-to-fire-ng-change-with-filemodel-input-type-file
-app.directive("filesInput", function() {
-	return {
-		require: "ngModel",
-		link: function postLink(scope,elem,attrs,ngModel) {
-			elem.on("change", function(e) {
-				var files = elem[0].files;
-				ngModel.$setViewValue(files);
-			});
-		}
-	}
-});
-*/
-//https://stackoverflow.com/questions/17063000/ng-model-for-input-type-file-with-directive-demo
-/*
-app.directive("fileread", [function () {
-    return {
-        scope: {
-            fileread: "="
-        },
-        link: function (scope, element, attributes) {
-            element.bind("change", function (changeEvent) {
-                scope.$apply(function () {
-                    scope.fileread = changeEvent.target.files[0];
-                    // or all selected files:
-                    // scope.fileread = changeEvent.target.files;
-                });
-            });
-        }
-    }
-}]);
-*/
-
 
 app.directive("fileread", [function () {
     return {
